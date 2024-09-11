88bd78a37d640c42d711544df511b5d28edb1afd
miller110 <millerssantiago@gmail.com>
2024-09-11 10:03:45 -0300

    Manipulating elements with CSS and JS

--- day_11/14_09_2024.js
+++ day_11/14_09_2024.js
@@ -1,14 +1,23 @@
 const saudacoes = document.getElementById('saudacoes');
 const nome = document.getElementById('nome');
+const header = document.querySelector('header');
 
 const mudarCorBotao = document.getElementById('mudarCor');
 const mudarTextoBotao = document.getElementById('mudarTexto');
 
+let alternarTexto = true;
+
 mudarCorBotao.addEventListener('click', () => {
     const corAleatoria = `#${Math.floor(Math.random()* 16777215).toString(16)}`;
     saudacoes.style.color = corAleatoria;
+    header.style.backgroundColor = corAleatoria;
 });
 
 mudarTextoBotao.addEventListener('click', () => {
-    saudacoes.textContent = "Seja muito bem vindo ao meu repositório!";
+    if (alternarTexto){
+        saudacoes.textContent = "Seja muito bem vindo ao meu repositório!";
+    } else {
+        saudacoes.textContent = "Olá, eu sou Miller Santiago! Muito prazer!";
+    }
+    alternarTexto = !alternarTexto;
 });

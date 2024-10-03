fb19342a75524d1c4f829c78881cb5625bc05b73
miller110 <millerssantiago@gmail.com>
2024-10-03 13:58:32 -0300

    React-Introduction and installation (update 2)

--- day_31/src/App.jsx
+++ day_31/src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Instale o Node JS e pesquise sobre a criação de um projeto React com Vite, dê os primeiros no desenvolvimento do projeto através da criação do mesmo, via terminal
-      </h1>
+      <h1>Miller Santiago</h1>
     </>
   )
 }

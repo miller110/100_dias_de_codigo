0313a0ae473a6bed5aeb6bf83d00b8f2002d5263
miller110 <millerssantiago@gmail.com>
2024-10-03 13:54:05 -0300

    React-Introduction and installation

---
+++ day_31/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Instale o Node JS e pesquise sobre a criação de um projeto React com Vite, dê os primeiros no desenvolvimento do projeto através da criação do mesmo, via terminal
+      </h1>
+    </>
+  )
+}
+
+export default App

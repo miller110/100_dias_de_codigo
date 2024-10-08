0a3162ecd321ef7231126b6a3b1e1a26e29b1a48
miller110 <millerssantiago@gmail.com>
2024-10-08 20:57:53 -0300

    Creating a counter in react

---
+++ day_35/README.md
@@ -0,0 +1,7 @@
+# React + Vite
+<hr>
+
+#### Crie um componente no react referente à um contador, use o hook useState para adicionar funcionalidade a esse componente
+<hr>
+
+<img src="src/img/1.png">
---
+++ day_35/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import MyComponent from './components/MyComponent'
+import CondicionalComponent from './components/CondicionalComponent'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+      <div>
+        <Counter/>
+      </div>
+  )
+}
+
+export default App

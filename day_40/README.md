6d7daeb111e046259cea9c7f87d6f53db5a58cb8
miller110 <millerssantiago@gmail.com>
2024-10-13 23:28:23 -0300

    Task board in react- part 3 (incomplete task)

---
+++ day_40/README.md
@@ -0,0 +1,8 @@
+# React + Vite
+
+## Quadro de Tarefas em React – Parte 3
+
+#### Para concluir o projeto, crie um último componente referente ao quadro de nossa aplicação, onde as tarefas adicionadas pelo usuário serão listadas, lembre-se de manter a comunicação entre os componentes usando props. Adicione também ao projeto um botão de deletar em cada uma das tarefas, para a funcionalidade de apagar uma tarefa específica/concluída.
+<hr>
+
+
---
+++ day_40/src/App.jsx
@@ -0,0 +1,32 @@
+import { React, useState } from 'react';
+import Input from './components/Input';
+import Board from './components/Board';
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center py-8 gap-4'>
+        <h1>Quadro de Tarefas</h1>
+        <Input taskList={taskList} setTaskList={setTaskList} />
+      </div>
+
+      <div className='flex flex-wrap gap-4 mt-4'>
+        {taskList.map((task, index) => (
+          <Board
+            key={index}
+            task={task}
+            index={index}
+            taskList={taskList}
+            setTaskList={setTaskList} />
+        ))}
+      </div>
+
+    </>
+  )
+}
+
+export default App;
